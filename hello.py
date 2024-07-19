0918861d9840f71e045a62412ac145213e0d1f55
FrankMcC7 <92784869+FrankMcC7@users.noreply.github.com>
2024-07-19 20:54:32 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ python -m spacy download en_core_web_sm
 pip install spacy-*.whl
 import spacy
 nlp = spacy.load("en_core_web_sm")
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
